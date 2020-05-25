6a4d54ae4b346db104e85cea03d6ded5a0d9dee3
Nicole Liu <nicoleliu_@outlook.com>
2020-05-25 10:32:40 -0400

    Initial Commit

---
+++ SafetyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SafetyApp
+//
+//  Created by Liu, Bojun on 2020-05-25.
+//  Copyright © 2020 Liu, Nicole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
