10604c288a5fa8ae85afcaa15b600a74355ee80c
Nicole Liu <nicoleliu_@outlook.com>
2020-05-27 15:01:21 -0400

    Copied some assets from the Honolulu art tutorial, added a map view to main storyboard, centered on Toronto

--- SafetyApp/ViewController.swift
+++ SafetyApp/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import MapKit
+
 
 class ViewController: UIViewController {
-
+    
+    // outlet that connects map to controller
+    @IBOutlet private var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // set initial location to Toronto
+        let initialLocation = CLLocation(latitude: 43.669914, longitude: -79.39031)
+        mapView.centerToLocation(initialLocation)
     }
-
-
+    
+    
 }
+
+private extension MKMapView {
+    func centerToLocation(
+        _ location: CLLocation,
+        regionRadius: CLLocationDistance = 5000
+    ) {
+        let coordinateRegion = MKCoordinateRegion(
+            center: location.coordinate,
+            latitudinalMeters: regionRadius,
+            longitudinalMeters: regionRadius)
+        setRegion(coordinateRegion, animated: true)
+    }
+}
+
 
